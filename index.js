3a7a3b6c315c32f19f3a619876ae9a5c9ae096be
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-08-19 16:26:02 -0400

    initial commit, task 2 complete

--- index.js
+++ index.js
@@ -60,7 +60,26 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
   
+  fill(gallons){
+    this.tank += gallons;
+  }
+  drive(distance){
+    if (distance/this.milesPerGallon > this.tank){
+      distance = this.tank * this.milesPerGallon;
+      this.odometer += distance;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+    this.odometer += distance;
+    this.tank -= distance/this.milesPerGallon;
+  }
 }
 
 /*
